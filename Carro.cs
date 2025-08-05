5706d25b112a868b4ac024d4ef6c378448684afb
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-08-05 10:54:45 -0300

    Update Carro.cs

--- Carro.cs
+++ Carro.cs
@@ -1,20 +1,11 @@
-using System;
-
 class Carro{
-    // Atributos da classe carro
+    // Atributos
     private string marca = "";
     private string modelo = "";
     private int anoDeFabricacao = 0;
-
-    // Método para ligar o carro
-    public void Ligar(){
-        Console.WriteLine($"{marca} {modelo} está ligado!");
-    }
-
-    public void SetModelo(string novoModelo){
-        if(novoModelo != ""){
-            modelo = novoModelo;
-        }
+    // Método
+    public void LigarCarro(){
+        Console.WriteLine($"{marca} {modelo} ano {anoDeFabricacao} está ligando!");
     }
 
     public void SetMarca(string novaMarca){
@@ -22,14 +13,23 @@ class Carro{
             marca = novaMarca;
         }
     }
+    public void SetModelo(string novoModelo){
+        if(novoModelo != ""){
+            modelo = novoModelo;
+        }
+    }
+    public void SetAno(int novoAno){
+        if(novoAno >= 2010){
+            anoDeFabricacao = novoAno;
+        }
+    }
 }
-
 class Program{
     static void Main(string[] args){
         Carro carro_1 = new Carro();
         carro_1.SetMarca("Toyota");
         carro_1.SetModelo("Corolla");
-        //carro_1.anoDeFabricacao = 2016;
-        carro_1.Ligar();
+        carro_1.SetAno(2015);
+        carro_1.LigarCarro();
     }
 }
