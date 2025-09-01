e1fa6878d0abd96ff73be212483854dc56ad09b6
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-09-01 09:05:27 -0300

    Create TodosController.cs

---
+++ API/Controllers/TodosController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MinhaAPI.Controllers{
+    [ApiController]
+    [Route("todos")]
+    public class TodosController : ControllerBase{
+        private static readonly List<Todo> Todos = new();
+        private static int _nextId = 1;
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Create([FromBody] CreateTodoDto dto){
+            var todo = new Todo(_nextId++, dto.Title.Trim(), dto.Priority, false);
+        return Created($"/todos/{todo.Id}", todo);
+
+        }
+    }
+    public record class CreateTodoDto{
+        [Required, MinLength(3)]
+        public string Title {get;set;} = string.Empty;
+        [Range(0,5)]
+        public int Priority{get;set;}
+    }
+    public record Todo(int Id,string Title, int Priority, bool Done);
+}
