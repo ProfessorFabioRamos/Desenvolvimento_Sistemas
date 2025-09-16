740f30d4703d8cd74cfba8e9a8531ba295dd32bc
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-09-16 09:25:22 -0300

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,62 +1,45 @@
-using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace MinhaAPI.Controllers;
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+// Adicionar Controllers
+builder.Services.AddControllers();
 
-[ApiController]
-[Route("todos")]
-public class TodosController : ControllerBase{
-    private static readonly List<Todo> Todos = new();
-    private static int _nextId = 1;
-    // Get com a rota "todos", pega todos os "ToDos"
-    [HttpGet]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public ActionResult<IEnumerable<Todo>> GetAll()
-        => Ok(Todos);
-    
-    [HttpGet("{id:int}")] // Pega por Id
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public ActionResult<Todo> GetById(int id){
-        var todo = Todos.FirstOrDefault(t=>t.Id ==id);
-        return todo is null ? NotFound():Ok(todo);
-    }
+var app = builder.Build();
 
-    // Post com a rota "todos"
-    [HttpPost]
-    [ProducesResponseType(StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public IActionResult Create([FromBody] CreateTodoDto dto){
-        var todo = new Todo(_nextId++,dto.Title.Trim(), dto.Priority,false);
-        Todos.Add(todo);
-        return Created($"/todos/{todo.Id}",todo);
-    }
-
-    [HttpPut("{id:int}")] // atualiza substituindo completamente
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult Replace(int id, [FromBody] UpdateTodoDto dto){
-        var idx = Todos.FindIndex(t=> t.Id ==id);
-        if(idx <0) return NotFound();
-
-        var updated = new Todo(id,dto.Title.Trim(),dto.Priority,dto.Done);
-        Todos[idx] = updated;
-        return Ok(updated);
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
 }
-public record class CreateTodoDto{
-    [Required, MinLength(3)]
-    public string Title {get;init;} = string.Empty;
-    [Range(0,5)]
-    public int Priority{get;init;}
+
+app.UseHttpsRedirection();
+// Adicionar
+app.MapControllers();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
-public record class UpdateTodoDto{
-    [Required, MinLength(3)]
-    public string Title {get;init;} = string.Empty;
-    [Range(0,5)]
-    public int Priority{get;init;}
-    public bool Done{get;init;}
-}
-public record Todo(int Id,string Title, int Priority, bool Done);
