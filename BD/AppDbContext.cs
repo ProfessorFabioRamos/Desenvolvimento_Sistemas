2532a509ffb88d8a402a1f78084a2a339d7af5c5
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-09-19 09:46:08 -0300

    Create AppDbContext.cs

---
+++ BD/AppDbContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Escola.Models;
+
+namespace Escola.Data;
+
+public class AppDbContext : DbContext{
+    public  DbSet<Student> Students => Set<Student>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=escola.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(e =>{
+            e.HasKey(s=>s.Id);
+            e.Property(s=>s.Name).IsRequired().HasMaxLength(120);
+            e.Property(s=>s.Email).IsRequired().HasMaxLength(100);
+            e.HasIndex(s=>s.Email).IsUnique(); // email único
+        });
+    }
+}
+
