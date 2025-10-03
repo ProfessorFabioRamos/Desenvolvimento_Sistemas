8a1f9f165f8ebaff52b620183f2d3f226d1d40b9
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-10-03 11:00:11 -0300

    Update AppDbContext.cs

--- BD/AppDbContext.cs
+++ BD/AppDbContext.cs
@@ -4,6 +4,9 @@ using Escola.Models;
 namespace Escola.Data;
 
 public class AppDbContext : DbContext{
+    public AppDbContext(){}
+    public AppDbContext(DbContextOptions<AppDbContext> options) :base(options){}
+
     public  DbSet<Student> Students => Set<Student>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
