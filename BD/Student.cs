d1b34d630669209dc6a6b9e3fe58e66522a6c197
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-10-10 10:05:54 -0300

    Delete BD/Student.cs

--- BD/Student.cs
+++
@@ -1,8 +0,0 @@
-namespace Escola.Models;
-
-public class Student{
-    public int Id {get;set;}
-    public string Name {get;set;} = "";
-    public string Email {get;set;} = "";
-    public DateTime EnrollmentDate {get;set;} = DateTime.UtcNow;
-}
