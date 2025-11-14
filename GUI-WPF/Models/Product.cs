ceeaea1902cd322994cd43d592354164f61ed391
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-11-14 09:17:14 -0300

    Update Product.cs

--- GUI-WPF/Models/Product.cs
+++ GUI-WPF/Models/Product.cs
@@ -1,6 +1,7 @@
 namespace CadastroProdutos.Models;
 
 public class Product{
+    public int Id {get;set;} // Obrigatório para o banco
     public string Name{get;set;} = "";
     public string Category{get;set;} = "";
     public float Price{get;set;} = 0;
