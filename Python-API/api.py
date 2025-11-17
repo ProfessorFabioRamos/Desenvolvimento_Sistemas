d8c11f25c2ced892970dab3c48ac9b48a6c33814
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-11-17 10:48:45 -0300

    Update api.py

--- Python-API/api.py
+++ Python-API/api.py
@@ -1,50 +1,52 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
 produtos = [
-    {"id":1,"nome":"Espada","preco":100},
-    {"id":2,"nome":"Escudo", "preco":150}
+    {"id":1, "nome":"Espada","preco":100.40},
+    {"id":2, "nome":"Escudo","preco":200.50},
+    {"id":3, "nome":"Armadura","preco":300.30}
 ]
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Bem-vindo"
+     return "Bem-vindo à Loja!"
 
-@app.route('/produto', methods=['GET'])
+@app.route("/produto", methods = ["GET"])
 def get_produtos():
     return jsonify(produtos)
 
-@app.route('/produto/<int:id>', methods=['GET'])
+@app.route("/produto/<int:id>", methods = ["GET"])
 def get_produto(id):
     for p in produtos:
         if p["id"] == id:
             return jsonify(p)
-    return jsonify({"erro":"produto nao encotrado"}),404
+    return jsonify({"erro": "produto não encontrado"}), 404
 
-@app.route('/produto/',methods = ['POST'])
+@app.route("/produto", methods = ["POST"])
 def adicionar_produto():
     novo = request.get_json()
     produtos.append(novo)
-    return jsonify(novo),201
+    return jsonify(novo), 201
 
-@app.route('/produto/<int:id>',methods=['PUT'])
+@app.route("/produto/<int:id>", methods = ["PUT"])
 def atualizar_produto(id):
-    dados = request.get_json()
+    dados_novo = request.get_json()
     for p in produtos:
-        if p["id"]==id:
-            p.update(dados)
+        if p["id"] == id:
+            p.update(dados_novo)
             return jsonify(p)
-    return jsonify({"erro":"Produto nao encontrado"}),404
+    return jsonify({"erro": "produto não encontrado"}), 404
 
-@app.route('/produto/<int:id>',methods = ['DELETE'])
+@app.route("/produto/<int:id>", methods = ["DELETE"])
 def deletar_produto(id):
     for p in produtos:
         if p["id"] == id:
             produtos.remove(p)
-            return jsonify({"mensagem":"Produto Removido"})
-    return jsonify({"erro":"Produto nao encontrado"}),404
+            return jsonify({"mensagem":"Produto Removido"}), 204
+    return jsonify({"erro": "produto não encontrado"}), 404
 
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run(debug=True)
 
 
@@ -52,9 +54,10 @@ if __name__=='__main__':
 
 # app = Flask(__name__)
 
+# #Home page
 # @app.route("/")
 # def home():
-#     return "API online!"
+#     return "Hello World!"
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
